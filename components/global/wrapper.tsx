b7b7c24e351b838daf26cfaa035729f4ecab4fda
Afzaal <afzaalbci@ymail.com>
2021-12-21 23:22:12 +0500

    fixed

--- components/global/wrapper.tsx
+++ components/global/wrapper.tsx
@@ -6,22 +6,22 @@ import { useRouter } from "next/router";
 const Wrapper = (props) => {
   var session = supabase.auth.session();
   const router = useRouter();
-  useEffect(() => {
-    if (session) {
-      router.pathname == "/" ? router.push("/admin") : null;
-    } else {
-    }
-    supabase.auth.onAuthStateChange((event, session) => {
-      console.log(event);
-      if (session && session.user) {
-        router.push("/admin");
-      }
+  // useEffect(() => {
+  //   if (session) {
+  //     router.pathname == "/" ? router.push("/admin") : null;
+  //   } else {
+  //   }
+  //   supabase.auth.onAuthStateChange((event, session) => {
+  //     console.log(event);
+  //     if (session && session.user) {
+  //       router.push("/admin");
+  //     }
 
-      if (!session) {
-        router.push("/");
-      }
-    });
-  }, [session]);
+  //     if (!session) {
+  //       router.push("/");
+  //     }
+  //   });
+  // }, [session]);
   return props.children;
 };
 
